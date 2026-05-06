ab1ded3f17a0da5cc4eaf81722c88b95df82fc24
davids199005 <davids199005@gmail.com>
2026-05-07 01:51:45 +0300

    chore: enhance main application with logging setup and database connection management

---
+++ Backend/app/core/database.py
@@ -0,0 +1,34 @@
+from pymongo.asynchronous.mongo_client import AsyncMongoClient
+from pymongo.asynchronous.database import AsyncDatabase
+from logging import Logger
+import logging
+from .config import config
+
+
+logger: Logger = logging.getLogger(name=__name__)
+
+client: AsyncMongoClient[dict[str, object]] | None = None
+database: AsyncDatabase[dict[str, object]] | None = None
+
+async def connect_to_database() -> None:
+    global client, database
+    client = AsyncMongoClient[dict[str, object]](uri=config.mongo_uri)
+
+    database = client.get_database(name=config.mongo_db_name)
+
+    _ = await client.admin.command("ping")
+    logger.info("Connected to MongoDB: %s", config.mongo_db_name)
+
+async def close_database_connection() -> None:
+    global client, database
+    if client is not None:
+        await client.close()
+        logger.info("Disconnected from MongoDB")
+    client = None
+    database = None
+
+
+def get_database() -> AsyncDatabase[dict[str, object]]:
+    if database is None:
+        raise RuntimeError("Database not connected")
+    return database
--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,11 +1,29 @@
-from fastapi import FastAPI
 
-app = FastAPI(
+
+import logging
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from .core.database import connect_to_database, close_database_connection
+
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+
+@asynccontextmanager
+async def lifespan(_app: FastAPI):
+    await connect_to_database()
+    yield
+    await close_database_connection()
+
+app: FastAPI = FastAPI(
     title="Inkstream",
     description="Inkstream is a ChatGPT style chat platform.",
 )
 
 
-@app.get("/")
+@app.get(path="/")
 async def root() -> dict[str, str]:
     return {"message": "Inkstream is a ChatGPT style chat platform Live now."}
