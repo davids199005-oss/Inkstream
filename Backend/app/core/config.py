b0d1ff4200c0ea95bacfc9cf71c411057ca37880
davids199005 <davids199005@gmail.com>
2026-05-07 00:58:12 +0300

    chore: update environment configuration and refactor config handling for improved clarity

--- Backend/app/core/config.py
+++ Backend/app/core/config.py
@@ -15,29 +15,28 @@ class Config:
     mongo_db_name: str
 
     # APP
-    app_env: str
     app_host: str
     app_port: int
     cors_origins: str
 
-def _required(value: str) -> str:
+def _required(name: str) -> str:
+    value: str | None = os.getenv(name)
     if not value:
         raise RuntimeError(
-            f"Missing required environment variable: {value}."
+            f"Missing required environment variable: {name}."
             +  "Please check your .env file and try again."
         )
     return value
 
 def get_config() -> Config:
     return Config(
-        openai_api_key=_required(value="OPENAI_API_KEY"),
-        openai_model=_required(value="OPENAI_MODEL"),
-        mongo_uri=_required(value="MONGO_URI"),
-        mongo_db_name=_required(value="MONGO_DB_NAME"),
-        app_env=_required(value="APP_ENV"),
-        app_host=_required(value="APP_HOST"),
-        app_port=int(_required(value="APP_PORT")),
-        cors_origins=_required(value="CORS_ORIGINS"),
+        openai_api_key=_required(name="OPENAI_API_KEY"),
+        openai_model=_required(name="OPENAI_MODEL"),
+        mongo_uri=_required(name="MONGO_DB_URI"),
+        mongo_db_name=_required(name="MONGO_DB_NAME"),
+        app_host=_required(name="HOST"),
+        app_port=int(_required(name="PORT")),
+        cors_origins=_required(name="CORS_ORIGINS"),
     )
 
 config: Config = get_config()
